b9324c9d95b017ee32df3544780e983c2d61d134
raushan187 <43811300+raushannn187@users.noreply.github.com>
2019-08-27 16:51:40 +0530

    intial commit

---
+++ src/app.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const geoCode = require ('./utils/geoCode');
+const forecast = require ('./utils/forecast');
+
+const app = express();
+
+// Define paths for Express configuration
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialPath = path.join(__dirname, "../templates/partials");
+
+// setup handlerbars engine and views directory
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialPath);
+
+// setup static directory to server
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Raushan Kr"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Raushan Kr"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Raushan Kr",
+    helpText: "click here for help!"
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address"
+    });
+  }
+
+  geoCode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error })
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error })
+      }
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address
+      })
+    });
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "Please Enter Search Value"
+    });
+  }
+  console.log(req.query.search);
+  res.send({
+    products: []
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Raushan Kr",
+    errorMsg: "help article not found!"
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Raushan Kr",
+    errorMsg: "Page not found!"
+  });
+});
+// app.com
+// app.com/help
+// app.com/about
+
+app.listen(5000, () => {
+  console.log("server run on port 5000");
+});
